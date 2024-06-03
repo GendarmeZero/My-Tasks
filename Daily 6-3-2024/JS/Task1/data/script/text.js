6c6c236d6365c516783e93bf8509ecfae7cf381f
gendarme_zero <alaabigsmoke@yahoo.com>
2024-06-03 03:21:36 +0300

    JS Task 1 is done

---
+++ Daily 6-3-2024/JS/Task1/data/script/text.js
@@ -0,0 +1,46 @@
+// 1
+let cash= 1000;
+let currentLiabilities= 500;
+let cashFlow = cash/currentLiabilities;
+console.log(cashFlow);
+// 2
+let revenues= 1000;
+let expenses= 500;
+let netIncome= revenues-expenses;
+console.log(netIncome);
+// 3
+let liabilities= 1000;
+let equity= 500;
+let totalAssets= liabilities+equity;
+console.log(totalAssets);
+// 4
+let profit= 1000;
+let sales= 500;
+let netIncome01= profit*sales;
+console.log(netIncome01);
+// 5
+let Average= (7+9+2)/3;
+console.log(Average);
+// 6
+let price= 150;
+let discount= 0.3;
+let discount01 = price - (price * discount);
+console.log(discount01);
+// 7
+let Age1=30
+let Age2=18
+if (Age1>20 && Age2<20) {
+    console.log(true);
+    
+}
+else{
+console.log(false);}
+// 8
+let exponential = 2**3
+console.log(exponential);
+// 9
+let remainder = 10%4
+console.log(remainder);
+
+
+
