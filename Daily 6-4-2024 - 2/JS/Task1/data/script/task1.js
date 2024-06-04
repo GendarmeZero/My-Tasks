5882a73623aaedc8be9546228cd3f3de78230bec
gendarme_zero <alaabigsmoke@yahoo.com>
2024-06-04 23:42:26 +0300

    JS Task2

---
+++ Daily 6-4-2024 - 2/JS/Task1/data/script/task1.js
@@ -0,0 +1,46 @@
+// Task1
+
+let sayHello=(name)=> {
+    let x="Hello,"+name+"!";
+    return x;
+}
+console.log(sayHello("Alice"));
+console.log(sayHello("Bob"));
+
+// Tasks2
+let square=function(sq){
+    let square=sq*sq;
+    return square;
+}
+console.log(square(2));
+console.log(square(5));
+// Task3
+let multiply=(a,b)=>{
+    let o=a*b;
+    return o;
+}
+console.log(multiply(2,3));
+console.log(multiply(4,5));
+// Task4
+// let makeCar=(make,model,year)=> {
+//     let build={
+//         make,
+//         model,
+//         year,
+//     }
+//     return build;
+// }
+// console.log(makeCar("car","car",10));
+
+let makeCar={
+        make:"Toyota",
+        model:"Camry", 
+        year:2020,
+        getCarInfo:()=>{
+            
+            console.log(makeCar.make + " " + makeCar.model + " " + makeCar.year) 
+            
+        }
+}
+console.log(makeCar.make);
+makeCar.getCarInfo()
