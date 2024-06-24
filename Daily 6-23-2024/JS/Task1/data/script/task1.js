fc38c0f34e71852502bcff8b2e493e6e7aa60a1b
gendarme_zero <alaabigsmoke@yahoo.com>
2024-06-24 09:37:03 +0300

    #

--- Daily 6-23-2024/JS/Task1/data/script/task1.js
+++ Daily 6-23-2024/JS/Task1/data/script/task1.js
@@ -6,7 +6,7 @@ class car {
     }
     carPrinter() {
         console.log(`The car has started, ${this.make}`);
-        
+
     };
 }
 
@@ -21,19 +21,61 @@ bmw.carPrinter();
 
 // Task 4
 class ElectricCar extends car {
-    constructor(make, model,battery_size) {
+    constructor(make, model, battery_size) {
         super(make, model);
         this.battery_size = battery_size;
     }
-    carBattery(){
+    carBattery() {
         return `${this.carPrinter()} with a baterry of ${this.battery_size}`
-        
+
     }
 }
-let bmw2024 = new ElectricCar('bmw', 'sport','large')
+let bmw2024 = new ElectricCar('bmw', 'sport', 'large')
 console.log(bmw2024.carBattery());
 
 // Task 5
 
+class bank {
+    #_balance = 0;
+    constructor(_balance) {
+        this.#_balance = _balance;
+    }
+    deposit(amount) {
+        if (amount > 0) {
+            this.#_balance += amount;
+        } else
+            console.log('please depose more then $5');
+
+    }
+    withdraw(amount) {
+        if (amount > 0 && amount <= this.#_balance) {
+            this.#_balance -= amount;
+        } else if (amount <= 0) {
+            console.log('withdraw must be postivie ');
+        } else {
+            console.error("Insufficient funds");
+
+        }
+    }
+    // getter 
+    getBalance() {
+        return this.#_balance;
+    }
+    setBalance(amount) {
+        if (amount >= 0) {
+            this.#_balance = amount;
+        } else {
+            console.error('ERROR')
+        }
+    }
+}
+
+const myAccount = new bank(100);
+myAccount.deposit(50);
+console.log(myAccount.getBalance());
+myAccount.withdraw(30);
+console.log(myAccount.getBalance());
 
 
+// Task 6 
+// Already done
