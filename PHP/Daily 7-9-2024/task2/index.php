7fce34c7193c3515310abfcf7c80ce8da456ba7e
gendarme_zero <alaabigsmoke@yahoo.com>
2024-07-11 14:58:43 +0300

    #

--- PHP/Daily 7-9-2024/task2/index.php
+++ PHP/Daily 7-9-2024/task2/index.php
@@ -11,6 +11,19 @@
             color: black;
             font-size: large;
         }
+
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+
+        th,
+        td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: center;
+        }
     </style>
 </head>
 
@@ -96,18 +109,106 @@
 
     echo "Factorial of $num is: $result";
 
+    // 5.Write a program to calculate and print the Fibonacci sequence using a for loop.
+    // Fibonacci is a series of numbers where a number is the sum of previous two numbers. Starting with 0 and 1, the sequence goes 0, 1, 1, 2, 3, 5, 8, 13, 21, and so on. 
+    // Expected Output: 0, 1, 1, 2, 3, 5, 8, 13, 21, …
+
     echo "<br>";
     echo "<br>";
-    echo 'Task 4 <br>';
+    echo 'Task 5 <br>';
+    echo '<hr>';
+
+    function Fibonacci($number)
+    {
+
+        if ($number == 0)
+            return 0;
+        else if ($number == 1)
+            return 1;
+
+        else
+            return (Fibonacci($number - 1) +
+                Fibonacci($number - 2));
+    }
+    $number = 10;
+    for ($counter = 0; $counter < $number; $counter++) {
+        echo Fibonacci($counter), ' ';
+    }
+
+    // 6.Write a program which will count the "c" characters in the text "Orange Coding Academy". 
+    // Sample Input: Orange Coding Academy
+    // Expected Output: 2
+
+
+    echo "<br>";
+    echo "<br>";
+    echo 'Task 6 <br>';
     echo '<hr>';
 
 
+    function findC($kokoOCA)
+    {
+        $koko = "Orange Coding Academy";
+        $count = 0;
+        for ($i = 0; $i < strlen($kokoOCA); $i++) {
+            if ($kokoOCA[$i] == 'c' || $kokoOCA[$i] == 'C') {
+                $count++;
+            }
+        }
+        return $count;
+    }
+    $kokoOCA = "Orange Coding Academy";
+    $cinme = findC($kokoOCA);
+    echo "$kokoOCA:$cinme";
 
 
+    // 7.Write a PHP script that creates the following table using for loops. Add cellpadding="3px" and cell spacing="0px" to the table tag. 
 
+    echo "<br>";
+    echo "<br>";
+    echo 'Task 7 <br>';
+    echo '<hr>';
 
+    $table = "<table border='1'>
+    <tr>
+        <th>Column 1</th>
+        <th>Column 2</th>
+    </tr>";
 
+    for ($i = 1; $i <= 6; $i++) {
+        $table .= "<tr>";
+        for ($j = 1; $j <= 2; $j++) {
+            $table .= "<td>Row $i, Column $j</td>";
+        }
+        $table .= "</tr>";
+    }
 
+    $table .= "</table>";
+
+    echo $table;
+
+    // 8.	Write a PHP program that repeats integers from 1 to 50. For multiples of three, print "koko" instead of the number, 
+    // and for multiples of five print "Buzz". For numbers that are multiples of both three and five, 
+    // print "FizzBuzz".
+
+    echo "<br>";
+    echo "<br>";
+    echo 'Task 8<br>';
+    echo '<hr>';
+
+    for ($i = 1; $i <= 50; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0 ) {
+            echo "kokoBuzz<br>";
+        }
+        if ($i % 3 == 0) {
+            echo "koko<br>";
+        };
+        if ($i % 5 == 0) {
+            echo "Buzz<br>";
+        }
+       
+        echo "$i <br>";
+    }
 
 
 
@@ -115,16 +216,18 @@
 
 
     ////////end of tasks
-
-
-
-
-
-
-
-
-
     ?>
+
+
+
+
+
+
+
+
+
+
+
 
 </body>
 
